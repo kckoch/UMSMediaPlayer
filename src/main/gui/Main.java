62b4cb7e870caa9d116ac395d29469268746a153
rmbsk_000@detrimenteagle <rbelile@g.clemson.edu>
2016-02-10 15:09:09 -0500

    initial try

---
+++ src/main/gui/Main.java
@@ -0,0 +1,29 @@
+package gui;
+
+import javax.swing.*;
+
+public class Main {
+	private static JFrame login;
+	private static JFrame mainframe;
+	private static User user;
+	private static Boolean loggedout;
+	
+	public static void main(String[] args) {
+		loggedout = false;
+		while(true) {
+			login = LoginGUI.init(user);
+			login.setVisible(true);
+			while(user == null) {
+				System.out.print("");
+			}
+			login.setVisible(false);
+			login.dispose();
+			
+			/*mainframe = MainFrame.init(loggedout);
+			while(!loggedout.booleanValue()) {
+				System.out.print("");
+			}*/
+		}
+	}
+
+}
