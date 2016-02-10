8db735e1becc0caba19a6cb7fe9cb8a36aefdd1b
Kyra Catherine Koch <kckoch@imp12.cs.clemson.edu>
2016-02-10 11:03:45 -0500

    Updating some gui stuff

--- src/gui/LoginGUI.java
+++ src/gui/LoginGUI.java
@@ -1,43 +1,68 @@
 package gui;
 
 import java.awt.*;
+import java.util.*;
+
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class LoginGUI {
 	private static JFrame frame;
+	private static User user;
+	private static ArrayList<User> users;
 	
-	public static JFrame init() {
+	public static JFrame init(User userIn) {
+		user = userIn;
+		
 		frame = new JFrame("Login");
     	frame.setSize(500, 600);
-    	frame.setBackground(Color.BLACK);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel logo = new JPanel(new GridBagLayout());
-		logo.setSize(500, 300);
-		logo.setBackground(Color.DARK_GRAY);
-		JLabel thatsmyjam = new JLabel("That's my jam!");
-		thatsmyjam.setMinimumSize(new Dimension(500, 300));
-		thatsmyjam.setForeground(Color.WHITE);
-		logo.add(thatsmyjam);
-		
+		JPanel logo = logo();
 		JPanel accounts = listAccounts();
+		JPanel loginbutton = loginButton();
 		
 		final JPanel windowPane = new JPanel(new BorderLayout());
 		
 		windowPane.add(logo);
 		windowPane.add(accounts);
+		windowPane.add(BorderLayout.SOUTH, loginbutton);
 		
 		frame.setBackground(Color.DARK_GRAY);
-		
 		frame.setContentPane(windowPane);
 		return frame;
 	}
 	
+	private static JPanel logo() {
+		Font font = new Font("That's my jam!", Font.ITALIC, 50);
+		
+		JPanel logo = new JPanel(new GridBagLayout());
+		logo.setSize(500, 200);
+		logo.setBackground(Color.DARK_GRAY);
+		JLabel thatsmyjam = new JLabel("That's my jam!");
+		thatsmyjam.setMinimumSize(new Dimension(500, 300));
+		thatsmyjam.setForeground(Color.WHITE);
+		thatsmyjam.setFont(font);
+		logo.add(thatsmyjam);
+		return logo;
+	}
+	
 	private static JPanel listAccounts() {
-		JPanel acc = new JPanel();
-		acc.setSize(500, 300);
+		JPanel acc = new JPanel(new BorderLayout());
+		acc.setSize(500, 200);
 		acc.setBackground(Color.DARK_GRAY);
 		return acc;
+	}
+	
+	private static JPanel loginButton() {
+		JPanel panel = new JPanel();
+		panel.setSize(500, 150);
+		panel.setBackground(Color.DARK_GRAY);
+		JButton loginbutton = new JButton("Login!");
+		loginbutton.setMaximumSize(new Dimension(50, 25));
+		loginbutton.setBorder(new EmptyBorder(50, 225, 50, 225));
+		panel.add(loginbutton);
+		return panel;
 	}
 }
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,22 +6,23 @@ public class Main {
 	private static JFrame login;
 	private static JFrame mainframe;
 	private static User user;
-	private static boolean loggedout;	//This is was a variable that I used to check when the user logged out
-										//Not sure what we'll do because we are separating all of the classes
-										//So we need to find out if we can check when a frame is set to invisible
+	private static Boolean loggedout;
 	
 	public static void main(String[] args) {
+		loggedout = false;
 		while(true) {
-			login = LoginGUI.init();
+			login = LoginGUI.init(user);
 			login.setVisible(true);
 			while(user == null) {
 				System.out.print("");
 			}
-			/*loggedout = false;
 			login.setVisible(false);
 			login.dispose();
 			
-			mainframe = MainFrame.init();*/
+			/*mainframe = MainFrame.init(loggedout);
+			while(!loggedout.booleanValue()) {
+				System.out.print("");
+			}*/
 		}
 	}
 
