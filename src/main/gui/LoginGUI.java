8a91413cd39e71b4d7887a158c7201d8f4d21170
Kyra Catherine Koch <kckoch@imp19.cs.clemson.edu>
2016-02-11 10:47:41 -0500

    works on login and maybe have an image panel?

--- src/main/gui/LoginGUI.java
+++ src/main/gui/LoginGUI.java
@@ -22,67 +22,59 @@ public class LoginGUI {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel logo = logo();
+		//Logo panel
+		Font font = new Font("That's my jam!", Font.ITALIC, 50);
+		JPanel logo = new JPanel();
+		logo.setLocation(0, 0);
+		logo.setSize(500, 149);
+		logo.setBackground(Color.DARK_GRAY);
 		
-		JPanel middle = new JPanel(new BorderLayout());
+		JPanel middle = new JPanel();
+		middle.setBounds(0, 184, 498, 357);
 		middle.setBackground(Color.DARK_GRAY);
-		middle.add(BorderLayout.PAGE_START, listAccounts());
-		middle.add(BorderLayout.PAGE_END, loginButton());
 		
-		JPanel copyright = copyright();
+		//login button
+		JButton loginbutton = new JButton("Login!");
+		loginbutton.setLocation(151, 311);
+		loginbutton.setSize(new Dimension(192, 34));
+		loginbutton.setBorder(null);
+		middle.setLayout(null);
+		middle.add(loginbutton);
 		
-		final JPanel windowPane = new JPanel(new BorderLayout());
+		final JPanel windowPane = new JPanel();
 		windowPane.setBackground(Color.DARK_GRAY);
 		
 		windowPane.add(BorderLayout.PAGE_START, logo);
-		windowPane.add(BorderLayout.CENTER, middle);
-		windowPane.add(BorderLayout.PAGE_END, copyright);
+		logo.setLayout(null);
+		
+		JLabel label_1 = new JLabel("That's my jam!");
+		label_1.setMaximumSize(new Dimension(500, 300));
+		label_1.setForeground(Color.WHITE);
+		label_1.setFont(new Font("Lobster Two", Font.ITALIC, 69));
+		label_1.setBorder(new EmptyBorder(50, 0, 50, 0));
+		label_1.setBounds(51, 12, 406, 159);
+		logo.add(label_1);
+		windowPane.setLayout(null);
+		windowPane.add(middle);
+		
+		JPanel admin = new JPanel();
+		admin.setBackground(Color.DARK_GRAY);
+		admin.setBounds(0, 0, 131, 163);
+		middle.add(admin);
+		admin.setBorder(null);
+		admin.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(12, 12, 107, 93);
+		admin.add(panel);
 		
 		frame.setBackground(Color.DARK_GRAY);
 		frame.setContentPane(windowPane);
-		return frame;
-	}
-	
-	private static JPanel logo() {
-		Font font = new Font("That's my jam!", Font.ITALIC, 50);
 		
-		JPanel logo = new JPanel(new GridBagLayout());
-		logo.setSize(500, 300);
-		logo.setBackground(Color.DARK_GRAY);
-		JLabel thatsmyjam = new JLabel("That's my jam!");
-		thatsmyjam.setMaximumSize(new Dimension(500, 300));
-		thatsmyjam.setForeground(Color.WHITE);
-		thatsmyjam.setFont(font);
-		thatsmyjam.setBorder(new EmptyBorder(50, 0, 50, 0));
-		logo.add(thatsmyjam);
-		return logo;
-	}
-	
-	private static JPanel listAccounts() {
-		JPanel acc = new JPanel(new BorderLayout());
-		acc.setSize(500, 200);
-		acc.setBackground(Color.DARK_GRAY);
-		return acc;
-	}
-	
-	private static JPanel loginButton() {
-		JPanel panel = new JPanel();
-		panel.setSize(500, 150);
-		panel.setBackground(Color.DARK_GRAY);
-		JButton loginbutton = new JButton("Login!");
-		loginbutton.setSize(new Dimension(100, 50));
-		loginbutton.setBorder(new EmptyBorder(50, 200, 50, 200));
-		panel.add(loginbutton);
-		return panel;
-	}
-	
-	private static JPanel copyright() {
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.DARK_GRAY);
-		panel.setSize(500, 50);
-		JLabel label = new JLabel("\u00a9 Team 14");
+		JLabel label = new JLabel("© Team 14");
 		label.setForeground(Color.WHITE);
-		panel.add(label);
-		return panel;
+		label.setBounds(204, 547, 86, 15);
+		windowPane.add(label);
+		return frame;
 	}
 }
