889864e1c9f0f3a22ad0fcd2e6c3d88387b101c0
kckoch <kochkyra1@gmail.com>
2016-02-16 14:40:49 -0500

    settings done?

--- src/main/gui/SettingGUI.java
+++ src/main/gui/SettingGUI.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Dimension;
 
 public class SettingGUI {
 	private static JPanel mainPanel = new JPanel();
@@ -14,6 +15,8 @@ public class SettingGUI {
 	 * @wbp.parser.entryPoint
 	 */
 	public static JPanel init(final JPanel mainPanelIn, final JFrame frame, final User user) {
+		mainPanel.setPreferredSize(new Dimension(500, 600));
+		mainPanel.setMaximumSize(new Dimension(500, 600));
 		mainPanel.setBackground(Color.DARK_GRAY);
 		mainPanel.setSize(500, 600);
 		mainPanel.setLayout(null);
@@ -46,7 +49,7 @@ public class SettingGUI {
 		
 		//exit button
 		JButton exitButton = new JButton("");
-		exitButton.setBounds(446, 15, 30, 30);
+		exitButton.setBounds(448, 5, 30, 30);
 		menupanel.add(exitButton);
 		exitButton.setBorder(null);
 		exitButton.setBackground(Color.DARK_GRAY);
@@ -73,11 +76,11 @@ public class SettingGUI {
 		usericon.setIcon(user.getSetting().getIcon());
 		
 		JLabel username = new JLabel("");
-		username.setFont(new Font("Droid Sans", Font.BOLD, 15));
+		username.setFont(new Font("Dialog", Font.BOLD, 21));
 		username.setForeground(Color.WHITE);
 		username.setBackground(Color.DARK_GRAY);
 		username.setText(user.getName());
-		username.setBounds(123, 36, 93, 25);
+		username.setBounds(123, 21, 118, 40);
 		userPanel.add(username);
 		
 		JButton btnChangePin = new JButton("Change Pin");
@@ -96,14 +99,64 @@ public class SettingGUI {
 		btnChangeProfilePic.setBounds(123, 100, 138, 25);
 		userPanel.add(btnChangeProfilePic);
 		
-		JScrollPane accPanel = new JScrollPane();
-		accPanel.setOpaque(false);
-		accPanel.setForeground(Color.DARK_GRAY);
-		accPanel.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		JPanel accPanel = new JPanel();
 		accPanel.setBorder(null);
 		accPanel.setBackground(Color.DARK_GRAY);
-		accPanel.setBounds(12, 232, 476, 344);
+		accPanel.setBounds(10, 231, 478, 358);
 		mainPanel.add(accPanel);
+		accPanel.setLayout(null);
+		
+		JLabel lblUsers = new JLabel("Users");
+		lblUsers.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsers.setForeground(Color.WHITE);
+		lblUsers.setFont(new Font("Dialog", Font.BOLD, 26));
+		lblUsers.setBackground(Color.DARK_GRAY);
+		lblUsers.setBounds(0, 0, 110, 34);
+		accPanel.add(lblUsers);
+		
+		JPanel exPanel = new JPanel();
+		exPanel.setBorder(null);
+		exPanel.setBackground(Color.DARK_GRAY);
+		exPanel.setBounds(10, 45, 458, 122);
+		accPanel.add(exPanel);
+		exPanel.setLayout(null);
+		
+		JLabel exIcon = new JLabel("New label");
+		exIcon.setIcon(new ImageIcon(SettingGUI.class.getResource("/main/gui/giraffe_blue.png")));
+		exIcon.setBorder(null);
+		exIcon.setBounds(10, 11, 93, 93);
+		exPanel.add(exIcon);
+		
+		JLabel lblTempUser = new JLabel("Temp User");
+		lblTempUser.setBackground(Color.DARK_GRAY);
+		lblTempUser.setForeground(Color.WHITE);
+		lblTempUser.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblTempUser.setBounds(114, 11, 121, 32);
+		exPanel.add(lblTempUser);
+		
+		JButton button = new JButton("Change Pin");
+		button.setForeground(Color.WHITE);
+		button.setFont(new Font("Dialog", Font.BOLD, 13));
+		button.setBorder(null);
+		button.setBackground(Color.GRAY);
+		button.setBounds(113, 46, 138, 25);
+		exPanel.add(button);
+		
+		JButton button_1 = new JButton("Change Profile Pic");
+		button_1.setForeground(Color.WHITE);
+		button_1.setFont(new Font("Dialog", Font.BOLD, 13));
+		button_1.setBorder(null);
+		button_1.setBackground(Color.GRAY);
+		button_1.setBounds(113, 79, 138, 25);
+		exPanel.add(button_1);
+		
+		JButton btnManageRestrictions = new JButton("Manage Restrictions");
+		btnManageRestrictions.setFont(new Font("Dialog", Font.BOLD, 13));
+		btnManageRestrictions.setForeground(Color.WHITE);
+		btnManageRestrictions.setBorder(null);
+		btnManageRestrictions.setBackground(Color.GRAY);
+		btnManageRestrictions.setBounds(261, 46, 138, 25);
+		exPanel.add(btnManageRestrictions);
 		
 		return mainPanel;
 	}
