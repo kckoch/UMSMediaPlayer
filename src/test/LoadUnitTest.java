1abb6c231220dd0b64716d6ffaedd03792146711
Genevieve <gmwatki@clemson.edu>
2016-02-17 17:39:36 -0500

    adding junit test cases into repository

---
+++ src/test/LoadUnitTest.java
@@ -0,0 +1,14 @@
+package src.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoadUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
