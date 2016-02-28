6e7d6c2706559ba4e5ef1ca4d7fda8a9b061afaf
kckoch <kckoch@clemson.edu>
2016-02-28 14:53:17 -0500

    SCROLLPANE WORKS

--- src/main/java/main/gui/Track.java
+++ src/main/java/main/gui/Track.java
@@ -9,13 +9,15 @@ public class Track {
 	int objectId;
 	String MediaURL;
 	Album album;
+	String artist;
 	
-	public Track(long duration, String trackTitle, String trackURL){
+	public Track(long duration, String trackTitle, String trackURL, String newArtist){
 		totalTime = duration;
 		title = trackTitle;
 		objectId = 0;
 		MediaURL = trackURL;
 		album = null;
+		artist = newArtist;
 	}
 	
 	public void setAlbum(Album newAlbum){
