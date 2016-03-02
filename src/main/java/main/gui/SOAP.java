d2f218a65385f74dba88e348a8db2754be7eae76
kckoch <kckoch@clemson.edu>
2016-03-02 13:10:53 -0500

    soap stuff, just commented out main

--- src/main/java/main/gui/SOAP.java
+++ src/main/java/main/gui/SOAP.java
@@ -1,7 +1,7 @@
 package main.gui;
 
-
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -57,12 +57,40 @@ public class SOAP {
 	static void processResults(String soapResponse) {
 		final String START_TAG = "<Result>";
 		final String END_TAG = "</Result>";
+		final String ID_START = "<container id=";
+		final String TITLE_START = "<dc:title>";
+		final String TITLE_END = "</dc:title>";
+		int id = 0;
+		String title = "";
 		int startIndex = soapResponse.indexOf(START_TAG) + START_TAG.length();
 		int endIndex = soapResponse.indexOf(END_TAG);
 		String resultStr = soapResponse.substring(startIndex, endIndex);
-		
 		resultStr = resultStr.replace("&lt;", "<").replace("&gt;",">");
 		
-		System.out.println(resultStr);		
+		StringTokenizer token = new StringTokenizer(resultStr, "<");
+		String tok = token.nextToken();
+		
+		while(token.hasMoreTokens()) {
+			if(tok.substring(0, 13).compareTo("container id=") == 0) {
+				id = Integer.parseInt(tok.substring(ID_START.length(), ID_START.length() + 2));
+			} 
+			if(tok.substring(0, 9).compareTo("dc:title>") == 0) {
+				title = tok.substring(TITLE_START.length()-1);
+			}
+			System.out.println(id + "\t" + title);
+			tok = token.nextToken();
+			/*
+			int idStartIndex = resultStr.indexOf(ID_START) + ID_START.length() + 1;
+			int idEndIndex = idStartIndex + 2;
+			int id = Integer.parseInt(resultStr.substring(idStartIndex, idEndIndex));
+			
+			int titleStartIndex = resultStr.indexOf(TITLE_START) + TITLE_START.length();
+			int titleEndIndex = resultStr.indexOf(TITLE_END);
+			String title = resultStr.substring(titleStartIndex, titleEndIndex);
+			
+			resultStr = resultStr.substring(titleEndIndex);
+			System.out.println("\n\n\n"+ resultStr.indexOf(ID_START) + ID_START.length() + 1);*/
+			
+		}
 	}
 }
