7e6868a9438b617a5e3a6f47c3792e00740e67f9
kckoch <kckoch@clemson.edu>
2016-03-05 21:02:36 -0500

    WORKS FOR SONGS BUT DOES NOT MAKE TRACKS

--- src/main/java/main/gui/MainFrame.java
+++ src/main/java/main/gui/MainFrame.java
@@ -41,7 +41,6 @@ public class MainFrame {
 	private static JTextField txtSearch;
 	private static JTable favoritesTable;
 	private static JTable libraryTable;
-	private static JScrollPane libraryPanel;
 	private static JPanel playPanel;
 	private static JLabel placeHolderForImage;
 	private static JFrame playFrame;
@@ -375,6 +374,7 @@ public class MainFrame {
 					libraryModel = new LibraryModel(list);
 					for(int j = 0; j < SOAP.getList().size(); j++) {
 						System.out.println(SOAP.getList().get(j).getId() + "\t" + SOAP.getList().get(j).getName());
+						if(user)
 						System.out.flush();
 					}
 					libraryModel.fireTableDataChanged();
